0499fb8094e739b780f905b59ee64784a9863297
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2019-07-10 00:07:29 +0300

    [implement-header]  - implement profile menu in header  - fix layout

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,17 +1,22 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 // Material modules
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+
 
 const MODULES = [
   MatSidenavModule,
   MatInputModule,
   MatIconModule,
   MatButtonModule,
+  MatMenuModule,
+  MatDividerModule,
 ];
 
 @NgModule({
