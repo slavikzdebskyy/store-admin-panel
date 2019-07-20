9879cf527753875819363eeb6e7c8b411c0a26d8
Andrew Makar <andrew.makar77@gmail.com>
2019-07-20 16:51:59 +0300

    Add cypress test for spinner on login & fix spinner.service.spec.ts

--- src/app/shared/services/spinner.service.spec.ts
+++ src/app/shared/services/spinner.service.spec.ts
@@ -3,7 +3,9 @@ import { TestBed } from '@angular/core/testing';
 import { SpinnerService } from './spinner.service';
 
 describe('SpinnerService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [SpinnerService]
+  }));
 
   it('should be created', () => {
     const service: SpinnerService = TestBed.get(SpinnerService);
