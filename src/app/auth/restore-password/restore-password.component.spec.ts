c4676137403ac3bc392b332d08455c63580fd892
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2019-08-15 00:22:39 +0300

    [implement-restore-password]  - fixed tests

--- src/app/auth/restore-password/restore-password.component.spec.ts
+++ src/app/auth/restore-password/restore-password.component.spec.ts
@@ -1,6 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RestorePasswordComponent } from './restore-password.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { TranslateToastrService } from 'src/app/services/translate-toastr.service';
+import { of } from 'rxjs';
+
+class AuthServiceMock {
+  public login() { return of({}); }
+}
 
 describe('RestorePasswordComponent', () => {
   let component: RestorePasswordComponent;
@@ -8,8 +18,17 @@ describe('RestorePasswordComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        RestorePasswordComponent,
+      declarations: [RestorePasswordComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        TranslateModule.forRoot(),
+      ],
+      providers: [
+        { provide: AuthService, useClass: AuthServiceMock },
+        { provide: TranslateToastrService },
       ],
     })
     .compileComponents();
